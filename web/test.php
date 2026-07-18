b21e00461e17e49d925e91d34fff29aa82bd6ebe
M Dzulfikri Alifajri <dzulfikrialifajri@gmail.com>
2026-07-18 14:00:33 +0700

    refactor: remove unused service integration and cleanup associated helper methods

--- web/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$request = Illuminate\Http\Request::create('/login', 'GET');
-$response = $kernel->handle($request);
-echo 'STATUS: ' . $response->getStatusCode() . "\n";
